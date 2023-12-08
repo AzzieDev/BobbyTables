5fde13499a7983e44559f4f9bc768a7afaf4a8db
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2023-12-07 20:59:14 -0500

    Create dbsecrets.sample.php
    Sample template for dbsecrets.php without the database info

---
+++ dbsecrets.sample.php
@@ -0,0 +1,14 @@
+<?php
+// Database Configuration
+	$hostname = "your_database_host";
+	$username = "your_database_username";
+	$password = "your_database_password";
+	$database = "your_database_name";
+
+// Create a database connection
+	$connection = mysqli_connect($hostname, $username, $password, $database);
+
+// Check the connection
+	if (!$connection) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
