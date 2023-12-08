04032e0924a6c3e59acc943cee8b0ae478f27936
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2023-12-07 22:50:51 -0500

    Add buttons to populate the input box

--- process.php
+++ process.php
@@ -1,58 +1,29 @@
-<?php
-require_once('dbsecrets.php');
+<!DOCTYPE html>
+<html>
 
-//retrieve from the form for further processing without sanitization
-$student_name = $_POST['student_name'];
+<head>
+    <title>SQL Injection Demo</title>
+</head>
 
-//allow us to regenerate the table if we wiped it :)
-if ($student_name == "reset") {
-    $query = "
-        DROP TABLE IF EXISTS Students;
-        
-        CREATE TABLE Students (
-            first VARCHAR(50)
-        );
-        
-        INSERT INTO Students VALUES
-        ('Alice'),
-        ('Bob'),
-        ('Charlie'),
-        ('David'),
-        ('Eve');
-    ";
+<body>
+    <form id="form" action="process.php" method="post">
+        <label for="student_name">Enter Student Name:</label>
+        <input type="text" id="student_name" name="student_name" value=""><br>
+        <input type="submit" value="Submit">
+    </form>
+    <button onclick="populateForm(1)">Robert\'); DROP TABLE Students; -- </button>
+    <button onclick="populateForm(2)">Populate/Reset Students table</button>
+    <script>
+        //passes data to the text field without submitting
+        function populateForm(option) {
+            let data = "";
+            if (option === 1) {
+                data = "'Robert'); DROP TABLE Students; -- '";
+            } else if (option === 2) {
+                data = "reset";
+            }
+            document.getElementById("student_name").value = data;
+        } </script>
+</body>
 
-    $result = mysqli_multi_query($connection, $query);
-
-    while (mysqli_next_result($connection)) {
-        if ($result = mysqli_store_result($connection)) {
-            mysqli_free_result($result);
-        }
-    }
-} else {
-
-    // Unsafe query prone to SQL injection
-    //$unsafe_query = "INSERT INTO `Students` VALUES ('Robert'); -- DROP TABLE Students;";
-
-    $unsafe_query = "INSERT INTO `Students` VALUES ('$student_name');";
-    //$result = $connection->multi_query($unsafe_query);
-
-    echo $unsafe_query . "<br><br>";
-    $result = mysqli_multi_query($connection, $unsafe_query);
-    // Discard the results without fetching or processing them
-    while (mysqli_next_result($connection)) {
-        if ($result = mysqli_store_result($connection)) {
-            mysqli_free_result($result);
-        }
-    }
-}
-$query = "SELECT * FROM Students";
-$result = mysqli_query($connection, $query);
-
-// Fetch data
-while ($row = mysqli_fetch_assoc($result)) {
-    // Display student data (for demonstration)
-    echo "Name: " . $row['first'] . "<br>";
-}
-
-// Close statement and connection
-mysqli_close($connection);
+</html>
