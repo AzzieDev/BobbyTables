3877ba992574bad36aa8c1cd9472537d28a4c4d5
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2023-12-08 04:47:00 -0500

    Indentation fix

--- dbsecrets.sample.php
+++ dbsecrets.sample.php
@@ -1,14 +1,14 @@
 <?php
 // Database Configuration
-	$hostname = "your_database_host";
-	$username = "your_database_username";
-	$password = "your_database_password";
-	$database = "your_database_name";
+$hostname = "your_database_host";
+$username = "your_database_username";
+$password = "your_database_password";
+$database = "your_database_name";
 
 // Create a database connection
-	$connection = mysqli_connect($hostname, $username, $password, $database);
+$connection = mysqli_connect($hostname, $username, $password, $database);
 
 // Check the connection
-	if (!$connection) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
+if (!$connection) {
+	die("Connection failed: " . mysqli_connect_error());
+}
